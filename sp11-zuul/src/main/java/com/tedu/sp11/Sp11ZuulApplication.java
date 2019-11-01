7b085333e117815b7193d3be36545fe8effc1576
000 <000@192.168.14.82>
2019-11-01 11:32:20 +0800

    123

---
+++ sp11-zuul/src/main/java/com/tedu/sp11/Sp11ZuulApplication.java
@@ -0,0 +1,13 @@
+package com.tedu.sp11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp11ZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sp11ZuulApplication.class, args);
+	}
+
+}
---
+++ sp11-zuul/src/main/java/com/tedu/sp11/filter/AccessFilter.java
@@ -0,0 +1,35 @@
+package com.tedu.sp11.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+public class AccessFilter extends ZuulFilter {
+
+	@Override
+	public boolean shouldFilter() {
+		
+		RequestContext currentContext = RequestContext.getCurrentContext();
+		
+		return false;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int filterOrder() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
